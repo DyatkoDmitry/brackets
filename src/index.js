0fd932bd7ed472e142e207ffdba6f659ad737115
DyatkoDmitry <dyatkodmitry@gmail.com>
2022-02-13 23:19:27 +0300

    feat: my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // Variable pairsArray for keeping bracket pairs
+  let pairsArray = [];
+  bracketsConfig.forEach(element => {
+      pairsArray.push(element.join(''));
+  });
+  
+  // Variable checked Pair for make sure lack of current bracket pair
+  let checkedPair = 0;
+  while( checkedPair < pairsArray.length){
+    pairsArray.forEach(element => {
+      while(str.indexOf(element) !== -1){
+          str = str.replace(element, ''); 
+          checkedPair = 0;    
+      }
+      checkedPair++;
+    })
+  }
+  return (str.length === 0) ? true : false;
 }
+
+
